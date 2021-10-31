f6cf80d85be7adaa7075f83d755d45d9e2a3af8d
Majenko Technologies <matt@majenko.co.uk>
2021-10-31 15:27:18 +0000

    Removed redundant comments

--- AmiMouse.ino
+++ AmiMouse.ino
@@ -1,11 +1,3 @@
-/**
- * Reads X/Y values from a PS/2 mouse connected to an Arduino
- * using the PS2Mouse library available from
- *   http://github.com/kristopher/PS2-Mouse-Arduino/
- * Original by Kristopher Chambers <kristopher.chambers@gmail.com>
- * Updated by Jonathan Oxer <jon@oxer.com.au>
- */
-
 #include <PS2Mouse.h>
 #define MOUSE_DATA 5
 #define MOUSE_CLOCK 6
